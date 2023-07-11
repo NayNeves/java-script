a4aa1c55173e598ee2b0dec8fbfffafe6b73cdb6
Nayara de Oliveira Neves <108471385+NayNeves@users.noreply.github.com>
2023-07-10 22:44:01 -0300

    curso de js

---
+++ javascript para iniciantes/variaveis/script.js
@@ -0,0 +1,15 @@
+var nome = 'Nayara';
+var idade = 26;
+var possuifaculdade = true;
+
+var preco = 50;
+var totalcomprado = 20
+var precototal = preco * totalcomprado;
+
+var cidade = 'Barueri', 
+    habitantes = '250000', 
+    quantidadecachorros = '3800';
+
+console.log (cidade, habitantes, quantidadecachorros)
+
+
